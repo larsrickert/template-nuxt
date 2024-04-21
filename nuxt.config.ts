10e7ea29ffa220304eb87647db2e5c2da6b2da98
Lars Rickert <dev@lars-rickert.de>
2024-04-21 15:15:44 +0200

    chore: setup i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    langDir: "lang",
+    defaultLocale: "en",
+    locales: [{ code: "en", file: "en-US.json" }],
+  },
 });

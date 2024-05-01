b36d0ccfec9c8ac4ac5efabff0396282b71dbeb0
Lars Rickert <dev@lars-rickert.de>
2024-05-01 19:32:52 +0200

    test: setup Vitest unit tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/i18n", "@nuxt/eslint"],
+  modules: ["@nuxtjs/i18n", "@nuxt/eslint", "@nuxt/test-utils/module"],
   css: ["~/assets/css/main.scss"],
   i18n: {
     lazy: true,

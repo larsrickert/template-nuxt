a23ceefeb3a2e96e838fae9d097dd53a077e44a1
Lars Rickert <dev@lars-rickert.de>
2024-07-23 20:28:48 +0200

    chore: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  compatibilityDate: "2024-07-23",
   modules: ["@nuxtjs/i18n", "@nuxt/eslint", "@nuxt/test-utils/module"],
   css: ["~/assets/css/main.scss"],
   i18n: {
     lazy: true,
     langDir: "lang",
-    defaultLocale: "en",
-    locales: [{ code: "en", file: "en-US.json" }],
+    defaultLocale: "en-US",
+    locales: [{ code: "en-US", file: "en-US.json" }],
   },
   nitro: {
     compressPublicAssets: true,

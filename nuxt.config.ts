77b0e172877dedb1c56e56b419d8ce9bdfc1735c
Lars Rickert <dev@lars-rickert.de>
2025-06-28 18:35:09 +0200

    chore: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: "2025-06-28",
   devtools: { enabled: true },
   typescript: { typeCheck: "build" },
   modules: ["@nuxtjs/i18n", "@nuxt/eslint", "@nuxt/test-utils/module"],

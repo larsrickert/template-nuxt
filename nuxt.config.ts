5fc6642b0efba000505db3c2a55b7eb6f9cde556
Lars Rickert <dev@lars-rickert.de>
2025-07-27 11:44:27 +0200

    chore: update to Nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,8 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/i18n", "@nuxt/eslint", "@nuxt/test-utils/module"],
   css: ["~/assets/css/main.scss"],
   i18n: {
-    lazy: true,
     defaultLocale: "en-US",
     locales: [{ code: "en-US", file: "en-US.json" }],
-    bundle: {
-      // see: https://github.com/nuxt-modules/i18n/issues/3238#issuecomment-2672492536
-      optimizeTranslationDirective: false,
-    },
   },
   nitro: {
     compressPublicAssets: true,
